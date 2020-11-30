3233cb77383565848b74b609cf3242b3628f7b03
fatemehmoradihaghighi <ghazalgimo@gmail.com>
2020-11-30 17:26:27 +0330

    remove main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import json
-import csv
-filename = "C:\\Users\ghazal\Desktop\googleplaystore.csv"
-filename2 = "C:\\Users\ghazal\Desktop\file.json"
-f = open(filename2, "a")
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        f.write("\n" + line)
-f.close()
